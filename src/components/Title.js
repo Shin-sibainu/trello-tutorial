aa2c727549986a5e26e03cd6e20c3fb0afd7a2a1
Shin-sibainu <shincode0712@gmail.com>
2021-08-04 15:48:13 +0900

    finish list components

---
+++ src/components/Title.js
@@ -0,0 +1,54 @@
+import { InputBase, Typography } from "@material-ui/core";
+import React, { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import FormatListBulletedIcon from "@material-ui/icons/FormatListBulleted";
+const useStyle = makeStyles((theme) => ({
+  editableTitleContainer: {
+    margin: theme.spacing(1),
+    display: "flex",
+  },
+  editableTitle: {
+    flexGrow: 1,
+  },
+  input: {
+    margin: theme.spacing(1),
+    "&:focus": {
+      background: "#ddd",
+    },
+  },
+}));
+
+export default function Title() {
+  const classes = useStyle();
+  const [open, setOpen] = useState(false);
+  return (
+    <div>
+      {open ? (
+        <div>
+          <InputBase
+            value="Todo"
+            inputProps={{
+              className: classes.input,
+            }}
+            fullWidth
+            onBlur={() => {
+              setOpen(!open);
+            }}
+          />
+        </div>
+      ) : (
+        <div className={classes.editableTitleContainer}>
+          <Typography
+            onClick={() => {
+              setOpen(!open);
+            }}
+            className={classes.editableTitle}
+          >
+            Todo
+          </Typography>
+          <FormatListBulletedIcon />
+        </div>
+      )}
+    </div>
+  );
+}
